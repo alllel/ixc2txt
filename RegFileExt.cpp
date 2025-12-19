caa5315256bcc314b33840d0fb901b445eb2c60e
Alexander Lelyakin <Alexander.Lelyakin@googlemail.com>
2025-12-19 11:48:00 +0100

    small rearrangement of code

--- RegFileExt.cpp
+++ RegFileExt.cpp
@@ -6,24 +6,27 @@
 #ifdef __MINGW32__
 #include "RegKey.hpp"
 
-bool RegisterFileExt() {
-    char buf[MAX_PATH];
+bool RegisterFileExt()
+{
+  {
+    RegKey key{R"(Software\Classes\.ixc)"};
+    if (!key) return false;
+    if (!key.SetDefStr("dxw32_file")) return false;
+  }
+  {
+    RegKey key{R"(Software\Classes\dxw32_file)"};
+    if (!key) return false;
+    if (!key.SetDefStr("Dxw32 file")) return false;
+  }
+  {
+    RegKey open{R"(Software\Classes\dxw32_file\shell\ixc2txt\command)"};
+    if (!open) return false;
+    char buf[MAX_PATH + 3];
     GetModuleFileName(nullptr, buf, MAX_PATH);
-    auto fn_len = std::strlen(buf); {
-        RegKey key{R"(Software\Classes\.ixc)"};
-        if (!key) return false;
-        if (!key.SetDefStr("dxw32_file")) return false;
-    } {
-        RegKey key{R"(Software\Classes\dxw32_file)"};
-        if (!key) return false;
-        if (!key.SetDefStr("Dxw32 file")) return false;
-    } {
-        RegKey open{R"(Software\Classes\dxw32_file\shell\ixc2txt\command)"};
-        if (!open) return false;
-        std::strcpy(buf + fn_len, " %1");
-        if (!open.SetDefStr(buf)) return false;
-    }
-    return true;
+    std::strcat(buf, " %1");
+    if (!open.SetDefStr(buf)) return false;
+  }
+  return true;
 }
 #else
 bool RegisterFileExt() { return true; }
